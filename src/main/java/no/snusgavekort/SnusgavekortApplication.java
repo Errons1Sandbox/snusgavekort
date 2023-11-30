d14942631a9b4749078e080cdd4246195578c0e5
Snorre Ledal <snorreledal@hotmail.no>
2023-11-30 17:48:43 +0100

    Initial commit

---
+++ src/main/java/no/snusgavekort/SnusgavekortApplication.java
@@ -0,0 +1,13 @@
+package no.snusgavekort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnusgavekortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnusgavekortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/snusgavekort/SnusgavekortApplicationTests.java
@@ -0,0 +1,13 @@
+package no.snusgavekort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnusgavekortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
